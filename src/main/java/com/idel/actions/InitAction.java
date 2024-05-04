c6ecb38fb1f24620fafcb3831877f5651bd5d7b8
Ideliya Safiullina <ideliya3004@gmail.com>
2024-05-04 23:17:59 +0300

    feat: implement InitAction

--- src/main/java/com/idel/actions/InitAction.java
+++ src/main/java/com/idel/actions/InitAction.java
@@ -6,14 +6,14 @@ import com.idel.entities.*;
 import java.util.Random;
 
 public class InitAction {
-    private Map map;
+    private WorldMap worldMap;
     private final int width;
     private final int height;
 
-    public InitAction(Map map) {
-        this.map = map;
-        this.width = map.getWidth();
-        this.height = map.getHeight();
+    public InitAction(WorldMap worldMap) {
+        this.worldMap = worldMap;
+        this.width = worldMap.getWidth();
+        this.height = worldMap.getHeight();
 
         setCreatures();
         setStaticEntities();
@@ -22,7 +22,7 @@ public class InitAction {
 
     private void setCreatures() {
         Random random = new Random();
-        int numberOfCreatures = (int) (width * height * 0.1);
+        int numberOfCreatures = (int) (width * height * 0.2);
         int x, y;
         for (int i = 0; i < numberOfCreatures; i++) {
             Coordinates coordinates;
@@ -30,15 +30,15 @@ public class InitAction {
                 x = random.nextInt(width);
                 y = random.nextInt(height);
                 coordinates = new Coordinates(x, y);
-            } while (!map.isCoordinateEmpty(coordinates));
+            } while (!worldMap.isCoordinateEmpty(coordinates));
 
             int typeOfStaticEntity = random.nextInt(2);
             switch (typeOfStaticEntity) {
                 case 0:
-                    map.setEntity(new Herbivore(coordinates), coordinates);
+                    worldMap.setEntity(new Herbivore(coordinates), coordinates);
                     break;
                 case 1:
-                    map.setEntity(new Predator(coordinates), coordinates);
+                    worldMap.setEntity(new Predator(coordinates), coordinates);
                     break;
             }
         }
@@ -46,7 +46,7 @@ public class InitAction {
 
     private void setStaticEntities() {
         Random random = new Random();
-        int numberOfStatics = (int) (width * height * 0.6);
+        int numberOfStatics = (int) (width * height * 0.4);
         int x, y;
         for (int i = 0; i < numberOfStatics; i++) {
             Coordinates coordinates;
@@ -54,18 +54,18 @@ public class InitAction {
                 x = random.nextInt(width);
                 y = random.nextInt(height);
                 coordinates = new Coordinates(x, y);
-            } while (!map.isCoordinateEmpty(coordinates));
+            } while (!worldMap.isCoordinateEmpty(coordinates));
 
             int typeOfStaticEntity = random.nextInt(3);
             switch (typeOfStaticEntity) {
                 case 0:
-                    map.setEntity(new Tree(coordinates), coordinates);
+                    worldMap.setEntity(new Tree(coordinates), coordinates);
                     break;
                 case 1:
-                    map.setEntity(new Rock(coordinates), coordinates);
+                    worldMap.setEntity(new Rock(coordinates), coordinates);
                     break;
                 case 2:
-                    map.setEntity(new Grass(coordinates), coordinates);
+                    worldMap.setEntity(new Grass(coordinates), coordinates);
                     break;
             }
         }
@@ -75,8 +75,8 @@ public class InitAction {
         for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
                 Coordinates coordinates = new Coordinates(x, y);
-                if (map.isCoordinateEmpty(coordinates))
-                    map.setEntity(new Dirt(coordinates), coordinates);
+                if (worldMap.isCoordinateEmpty(coordinates))
+                    worldMap.setEntity(new Dirt(coordinates), coordinates);
             }
         }
     }
